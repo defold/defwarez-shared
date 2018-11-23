77b920f892c3b347f3f38885a9204cde001598f1
Sven Andersson <sven.andersson@king.com>
2018-11-23 14:57:53 +0100

    Started on mock collection.

---
+++ defwarez-shared/game.go
@@ -0,0 +1,30 @@
+components {
+  id: "game"
+  component: "/defwarez-shared/game.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "game-settings"
+  component: "/defwarez-shared/game-settings.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
