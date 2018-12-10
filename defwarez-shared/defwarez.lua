31d1d0a3aacabd3b738115a70757d8d5e84847ac
Sven Andersson <sven.andersson@king.com>
2018-12-10 14:25:57 +0100

    Updated the GUI mock.

---
+++ defwarez-shared/defwarez.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.add_score = function(score)
+    
+    -- TODO
+    
+end
+
+M.set_score = function(abs_score)
+
+    -- TODO
+
+end
+
+M.get_players = function()
+
+    -- TODO
+    return {}
+end
+
+return M
