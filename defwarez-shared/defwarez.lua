938ced9b8ce7ddcc9dd1b8bba76c1d56b94d46f9
Sven Andersson <sven.andersson@king.com>
2018-12-10 15:45:43 +0100

    Added mocking check in defwarez.lua

--- defwarez-shared/defwarez.lua
+++ defwarez-shared/defwarez.lua
@@ -1,14 +1,24 @@
 local M = {}
 
+local is_live = sys.get_config("defwarez.live", "0") == "1"
+
 M.add_score = function(score)
-    
-    -- TODO
+
+    if is_live then
+        msg.post("main:/main", "add_score", { score = score } )
+    else
+        print("add score: " .. tostring(score))
+    end
     
 end
 
 M.set_score = function(abs_score)
 
-    -- TODO
+    if is_live then
+        msg.post("main:/main", "set_score", { score = abs_score } )
+    else
+        print("set score: " .. tostring(abs_score))
+    end
 
 end
 
